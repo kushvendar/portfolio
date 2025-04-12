3793c1e7718e0c62e1fa1c511fdcaaa4f10c8845
Kush <jangidkush1234@gmail.com>
2025-04-12 18:28:04 +0530

    navbar hero section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Image from "next/image";
 import Navbar from "./components/Navbar";
-import './globals.css'
+import './globals.css' 
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <Header/>
     </>
       );
 }
