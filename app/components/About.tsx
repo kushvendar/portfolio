e7c30e1bfd53fa9befb131db44f9e75a9646badf
Kush <jangidkush1234@gmail.com>
2025-04-24 14:34:39 +0530

    services portion done

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -15,11 +15,11 @@ const About = () => {
             <Image src={assets.user_image} alt='' className='w-full rounded-3xl'/>
           </div>
           <div className='flex-1'>
-            <p className='mb-10 max-w-2xl font-Ovo'>I am a experienced Full-stack developer with over a decaded of professional expertise in the field. Throughout my carrer, I had the privilege of collabrating with prestigious organisations, contributing to their suceess and growth.</p>
+            <p className='a'>I am a experienced Full-stack developer with over a decaded of professional expertise in the field. Throughout my carrer, I had the privilege of collabrating with prestigious organisations, contributing to their suceess and growth.</p>
 
             <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl '>
               {infoList.map(({icon, iconDark, title,description},index)=>(
-                <li key={index} className='border-[0.5px] border-gray-500 rounded-xl cursor-pointer p-6 hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-black'>
+                <li key={index} className='border-[0.5px] border-gray-500 rounded-xl cursor-pointer p-6 hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-lg'>
                   <Image src={icon} alt={title} className='w-7 mt-3'/>
                   <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
                   <p className='text-gray-500 text-sm '>{description}</p>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -37,12 +37,12 @@ const Navbar = () => {
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
         <Image src={assets.header_bg_color} alt='gradient' className='w-full'/>
     </div>
-    <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4  flex items-center justify-between z-50 `}>
+    <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4  flex items-center justify-between z-50 ${isScroll?"bg-white-opacity-50 backdrop-blur-lg shadow-sm":""} `}>
         <a href="#top">
             <Image src={assets.logo} alt='image' className='w-28 mr-14 cursor-pointer '/>
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll?"":"bg-white shadow-sm bg-opacity-50"}`}>
             <li><a className='font-Ovo' href="#top">Home</a></li>
             <li><a className='font-Ovo' href="#about">About me</a></li>
             <li><a className='font-Ovo' href="#services">Services</a></li>
