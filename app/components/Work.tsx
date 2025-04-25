f44c728942217668ea837b7fe1a31d476ad8e372
Kush <jangidkush1234@gmail.com>
2025-04-25 17:22:23 +0530

    my work section completed

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -10,16 +10,21 @@ const Work = () => {
       </p>
       <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] my-10 gap-5'>
         {workData.map(({title,description,bgImage},index)=>(
-          <div key={index} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' style={{backgroundImage: `url(${bgImage})`}}>
-            <div>
-              <h2>{title}</h2>
-              <p>{description}</p>
+          <div key={index} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' style={{backgroundImage: `url("../assets/public/work-1.png")`}}>
+            <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
+              <div>
+               <h2 className='font-semibold '>{title}</h2>
+               <p className='text-sm text-green-700 '>{description}</p>
               </div>  
-              <Image src={assets.send_icon} alt='send-icon' className='w-5'/>
+              <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
+                <Image src={assets.send_icon} alt='send-icon' className='w-5'/>
+              </div>
+             </div>
             </div>
         ))}
       </div>
-    </div>
+          <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] rounded-full py-3 px-10 mx-auto hover:bg-[#fcf4ff] duration-500'>Show more <Image src={assets.right_arrow_bold} alt='arrow' className='w-4' /></a>
+    </div> 
   )
 }
 
